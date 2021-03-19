c9a0fe7cc41a55e9f5e74c81a843b42388309b0d
Roland Montalvan <rolandfilho@gmail.com>
2021-03-19 09:24:11 -0300

    'primeira'

---
+++ client/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'src/contacts_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Contacts List',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ContactsScreen(title: 'Contacts List'),
+    );
+  }
+}
